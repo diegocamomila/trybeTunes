b3cba939cce552e6eb448be7b33755eab25408bb
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-16 14:06:01 -0300

    iniciado requisito 5 reaproveitado parte do codigo item 2

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,88 @@
 import React from 'react';
 import Header from '../components/Header';
+// import { createUser } from '../services/userAPI';
+// import { Redirect } from 'react-router-dom';
+// import Carregando from '../components/Carregando';
 
+const MIN_LENGTH = 2;
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputNameBand: '',
+      isDisabled: true,
+    //  loading: false,
+    //  redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validadteInput = this.validadteInput.bind(this);
+  }
+
+  // handleClick = () => {
+  //   const { inputName } = this.state;
+  //   this.setState({
+  //     loading: true,
+  //   });
+  //   createUser({ name: inputName }).then(() => {
+  //     this.setState({
+  //       loading: false,
+  //       redirect: true,
+  //     });
+  //   });
+  // }
+
+  handleChange({ name, value }) {
+    this.setState({
+      [name]: value,
+    }, () => this.validadteInput());
+    // quandoatualiza o estado é asincrono mas nao é uma promise, por isso tem de chamar
+    // a funçao de validaçao como calback junto coom o setState
+  }
+
+  validadteInput() {
+    const { inputNameBand } = this.state; // como vai fazer validaçoes precisa do valor do estado atualizado
+    const validadteNameInput = inputNameBand.length < MIN_LENGTH;
+
+    this.setState({
+      isDisabled: validadteNameInput,
+    });
+  }
+
   render() {
+    const {
+      inputNameBand,
+      isDisabled,
+      // loading,
+      // redirect,
+    } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="inputNameBand">
+            Nome da banda ou artista:
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="inputNameBand"
+              id="inputNameBand"
+              value={ inputNameBand }
+              onChange={ ({ target }) => this.handleChange(target) } // desetruturado o target de dentro do event, 14/01/22 mentoria de revisão Moises santana
+            />
+          </label>
+
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
